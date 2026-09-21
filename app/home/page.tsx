f89a6f4969b0ea80fac1e4d0c8265e492ff151ca
Evans-Munatsa <evansmunatsa7@gmail.com>
2026-09-21 10:23:55 +0200

    coming soon

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import React from 'react';
 import { HeroSection } from '@/components/home/HeroSection';
 import { QuoteAndPillars } from '@/components/home/QuoteAndPillars';
 import { JourneyAndWhoWeSupport } from '@/components/home/JourneyAndWhoWeSupport';
 import { ServicesCircle } from '@/components/home/ServicesCircle';
 import { FourStageApproach } from '@/components/home/FourStageApproach';
-import { PartnershipsAndReferral } from '@/components/home/PartnershipsAndReferral';
+// import { PartnershipsAndReferral } from '@/components/home/PartnershipsAndReferral';
 import { ResourcesAndCallToAction } from '@/components/home/ResourcesAndCallToAction';
 import { ServiceItem, FrameworkStage } from '@/types';
 
@@ -19,7 +21,7 @@ interface HomePageProps {
   onExploreServices: () => void;
 }
 
-export const HomePage: React.FC<HomePageProps> = ({
+export default function HomePage({
   onOpenReferral,
   onBookIntro,
   onLearnMore,
@@ -28,7 +30,7 @@ export const HomePage: React.FC<HomePageProps> = ({
   onSelectPillar,
   onVisitResources,
   onExploreServices,
-}) => {
+}: HomePageProps) {
   return (
     <div className="flex flex-col w-full">
       <HeroSection
@@ -54,78 +56,10 @@ export const HomePage: React.FC<HomePageProps> = ({
         onDiscoverMore={onDiscoverStage}
       />
 
-      <PartnershipsAndReferral
+      {/* <PartnershipsAndReferral
         onBookIntro={onBookIntro}
         onOpenReferral={() => onOpenReferral()}
-      />
-
-      <ResourcesAndCallToAction
-        onVisitResources={onVisitResources}
-        onOpenReferral={() => onOpenReferral()}
-      />
-    </div>
-  );
-};
-import React from 'react';
-import { HeroSection } from '@/components/home/HeroSection';
-import { QuoteAndPillars } from '@/components/home/QuoteAndPillars';
-import { JourneyAndWhoWeSupport } from '@/components/home/JourneyAndWhoWeSupport';
-import { ServicesCircle } from '@/components/home/ServicesCircle';
-import { FourStageApproach } from '@/components/home/FourStageApproach';
-import { PartnershipsAndReferral } from '@/components/home/PartnershipsAndReferral';
-import { ResourcesAndCallToAction } from '@/components/home/ResourcesAndCallToAction';
-import { ServiceItem, FrameworkStage } from '@/types';
-
-interface HomePageProps {
-  onOpenReferral: (serviceName?: string) => void;
-  onBookIntro: () => void;
-  onLearnMore: () => void;
-  onSelectService: (service: ServiceItem) => void;
-  onDiscoverStage: (stage: FrameworkStage) => void;
-  onSelectPillar: (pillarId: string) => void;
-  onVisitResources: () => void;
-  onExploreServices: () => void;
-}
-
-export const HomePage: React.FC<HomePageProps> = ({
-  onOpenReferral,
-  onBookIntro,
-  onLearnMore,
-  onSelectService,
-  onDiscoverStage,
-  onSelectPillar,
-  onVisitResources,
-  onExploreServices,
-}) => {
-  return (
-    <div className="flex flex-col w-full">
-      <HeroSection
-        onOpenReferral={() => onOpenReferral()}
-        onLearnMore={onLearnMore}
-      />
-
-      <QuoteAndPillars
-        onLearnMore={onLearnMore}
-        onSelectPillar={onSelectPillar}
-      />
-
-      <JourneyAndWhoWeSupport
-        onOpenReferral={() => onOpenReferral()}
-        onExploreServices={onExploreServices}
-      />
-
-      <ServicesCircle
-        onSelectService={onSelectService}
-      />
-
-      <FourStageApproach
-        onDiscoverMore={onDiscoverStage}
-      />
-
-      <PartnershipsAndReferral
-        onBookIntro={onBookIntro}
-        onOpenReferral={() => onOpenReferral()}
-      />
+      /> */}
 
       <ResourcesAndCallToAction
         onVisitResources={onVisitResources}
